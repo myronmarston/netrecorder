5fef6e4ac26e8ff902ad10f4708e9b6186e552dc
Myron Marston <myron.marston@gmail.com>
2010-01-24 22:13:45 -0800

    Gutted most of the code so I can re-design the API using my sandbox idea.

--- lib/net_recorder.rb
+++ lib/net_recorder.rb
@@ -2,68 +2,8 @@
 
 require 'yaml'
 require "net_recorder/fake_web_extensions"
-require "net_recorder/http"
-require "net_recorder/http_header"
 require "net_recorder/config"
 
 # NetRecorder - the global namespace
 module NetRecorder
-  
-  # the path to the cache file
-  def self.cache_file
-    @@config.cache_file
-  end
-  
-  def self.fakes
-    if File.exist?(@@config.cache_file)
-      File.open(@@config.cache_file, "r") do |f|
-        YAML.load(f.read)
-      end
-    end || []
-  end
-  
-  # configure netrecorder
-  def self.config
-    @@configured ||= nil
-    @@config = Config.new
-    yield @@config
-    record_net_calls
-    clear_cache!      if @@config.clear_cache
-    fakeweb           if @@config.fakeweb
-  end
-  
-  # returns true if record_net_calls is set to true in the config
-  def self.recording?
-    @@config.record_net_calls
-  end
-    
-  # save the fakes hash to the cash file
-  def self.cache!
-    File.open(@@config.cache_file, 'w') {|f| f.write Net::HTTP.fakes.to_yaml}
-  end
-  
-  # delete the cache file
-  def self.clear_cache!
-    if File.exist?(@@config.cache_file)
-      File.delete(@@config.cache_file)
-    end
-    Net::HTTP.clear_netrecorder_cache!
-  end
-  
-private
-  # load the cache and register all of the urls with fakeweb
-  def self.fakeweb
-    fakes.each do |fake|
-      FakeWeb.register_uri(fake[1][:method].downcase.to_sym, fake[0], fake[1][:body])
-    end
-  end
-  
-  # extend NET library to record requests and responses
-  def self.record_net_calls
-    return if @@configured
-    @@configured = true
-    
-    Net::HTTP.extend(NetHTTP)
-    Net::HTTPHeader.extend(NetHTTPHeader)
-  end
 end
