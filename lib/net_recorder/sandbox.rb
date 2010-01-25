27f18faea4645a72f1b9ab0a9bd7ba17d68efaf8
Myron Marston <myron.marston@gmail.com>
2010-01-25 13:40:31 -0800

    Ensure that @original_recorded_responses and @recorded_responses are separate array instances, so that #new_recorded_responses works properly.

--- lib/net_recorder/sandbox.rb
+++ lib/net_recorder/sandbox.rb
@@ -60,7 +60,8 @@ module NetRecorder
       return if record_mode == :all
 
       if cache_file
-        @original_recorded_responses = @recorded_responses = File.open(cache_file, 'r') { |f| YAML.load(f.read) } if File.exist?(cache_file)
+        @original_recorded_responses = File.open(cache_file, 'r') { |f| YAML.load(f.read) } if File.exist?(cache_file)
+        recorded_responses.replace(@original_recorded_responses)
       end
 
       recorded_responses.each do |rr|
